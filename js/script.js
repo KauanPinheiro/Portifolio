1978933ece9c50b525576932149441d3dced1cee
KauanPinheiro <kauan.pnascimento750@gmail.com>
2025-02-04 10:26:25 -0300

    atualizando umas tags

---
+++ js/script.js
@@ -0,0 +1,25 @@
+ScrollReveal().reveal('.txt-hero', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.txt-itens', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.txt-habilidades', {
+    origin: 'left',
+    duration: 1000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.icons-habilidades', {
+    origin: 'right',
+    duration: 1000,
+    distance: '20%'
+});
+ScrollReveal().reveal('.sobre-mim', {
+    origin: 'left',
+    duration: 1000,
+    distance: '20%'
+});
